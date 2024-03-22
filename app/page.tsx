5283f0e1efff50bd5bc7a5ba43a7ecbaa307de8a
Moon-Knighte <githubanjan2255@gmail.com>
2024-03-22 14:35:26 +0545

    adding loading and suspense component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
 import Link from "next/link";
 import Dashboard from "./dashboard/page";
+import { Suspense } from "react";
+import Loading from "./dashboard/loading";
 
 const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello</h1>
       <div>
-        <Link
-          className="font-medium text-sm hover:bg-slate-600 rounded-md border-[2px] border-slate-600 bg-slate-400 p-2"
-          href="/dashboard"
-        >
-          Dashboard
-        </Link>
+        <Suspense fallback={<Loading />}>
+          <Link
+            className="font-medium text-sm hover:bg-slate-600 rounded-md border-[2px] border-slate-600 bg-slate-400 p-2"
+            href="/dashboard"
+          >
+            Dashboard
+          </Link>
+        </Suspense>
         <Link
           className="font-medium ml-2 text-sm hover:bg-slate-600 rounded-md border-[2px] border-slate-600 bg-slate-400 p-2"
           href="/about"
