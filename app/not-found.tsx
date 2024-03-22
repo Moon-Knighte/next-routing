44fa4cda3f76fdb8302cc5fda1bb4870d36a78cd
Moon-Knighte <githubanjan2255@gmail.com>
2024-03-22 14:23:40 +0545

    Added page not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="h-screen text-4xl justify-center items-center p-24 text-red-600">
+      404
+      <hr />
+      Page Not Found...
+    </div>
+  );
+};
+
+export default NotFound;
