c736d1fa3ec8470d281a5dfba9425982b17b9155
Moon-Knighte <githubanjan2255@gmail.com>
2024-03-22 14:18:45 +0545

    Adding about and dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
+import Link from "next/link";
+import Dashboard from "./dashboard/page";
+
+const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello</h1>
+      <div>
+        <Link
+          className="font-medium text-sm hover:bg-slate-600 rounded-md border-[2px] border-slate-600 bg-slate-400 p-2"
+          href="/dashboard"
+        >
+          Dashboard
+        </Link>
+        <Link
+          className="font-medium ml-2 text-sm hover:bg-slate-600 rounded-md border-[2px] border-slate-600 bg-slate-400 p-2"
+          href="/about"
+        >
+          About
+        </Link>
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
